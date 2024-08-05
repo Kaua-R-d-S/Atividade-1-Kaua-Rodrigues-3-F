071d18901faa0d90c6ab39e6b730f5b05bc8de42
Kauã <126888652+Kaua-R-d-S@users.noreply.github.com>
2024-08-05 22:52:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Console } = require("console");
+
+console.log("Lucas stasiak é um dos mior professor de computador");    
+
+console.log ("O terceiro F pode ate não ser a melhor turma mas na cabeça deles eles são os MIOR ");
+
+const meuNome = "Kauã";
+
+console.log(meuNome)
+
+const idade= 18;
+
+console.log(idade);
+
+const altura = 1.70;
+
+console.log(altura);
+
+const massa = 75;
+
+console.log(massa);
+
+const cidade = "São José dos Pinhais";
+
+Console.log(cidade);
+
+// adicione nesse programa altura,massa e cidade onde mora 
